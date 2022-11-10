047f68a41041aa229bae3dcf5ff56f521c80f258
William Giovanni Cheverria Pacheco <cheverriawg@gmail.com>
2022-11-10 13:51:11 -0600

    modificando las rutas

--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,17 @@ import express, { NextFunction, Request, Response, Router } from 'express';
 require('dotenv').config();
 
 const app = express();
-const tareasRoutes = Router();
 const tareasBuscar = Router();
 let lista: string[];
 
 lista = ['Escribir', 'Estudiar'];
 
 app.get('/', (req: Request, res: Response) => {
-	res.send(
-		'Hola, ¿Quieres ver las tareas? => ' +
-			"<a href='/tareas'>" +
-			'ir a tareas' +
-			'</a>'
-	);
-});
-
-tareasRoutes.get('/', (req: Request, res: Response) => {
 	res.send(lista);
 });
 
 //para que funcione en el navegador o curl
-tareasRoutes.get('/:param', (req: Request, res: Response) => {
+app.get('/:param', (req: Request, res: Response) => {
 	const { param } = req.params;
 	lista.push(`${param}`);
 	//console.info(lista);
@@ -30,14 +20,14 @@ tareasRoutes.get('/:param', (req: Request, res: Response) => {
 });
 
 //funciona en postman
-tareasRoutes.post('/:param', (req: Request, res: Response) => {
+app.post('/:param', (req: Request, res: Response) => {
 	const { param } = req.params;
 	lista.push(`${param}`);
 	console.info(lista);
 	res.status(201).send(lista);
 });
 
-//
+//Buscar Tarea
 tareasBuscar.get('/:palabra', (req: Request, res: Response) => {
 	const { palabra } = req.params;
 	const result = lista.filter(valor => valor == palabra);
@@ -45,8 +35,7 @@ tareasBuscar.get('/:palabra', (req: Request, res: Response) => {
 	res.status(201).send(result);
 });
 
-app.use('/tareas', tareasRoutes);
-app.use('/tareas/search', tareasBuscar);
+app.use('/search', tareasBuscar);
 
 process.env.PORT
 	? console.log('localhost:' + process.env.PORT)
