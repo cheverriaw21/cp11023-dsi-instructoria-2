cac6a91f62d76b98aa9dfaaf032d97836db6d3d1
William Giovanni Cheverria Pacheco <cheverriawg@gmail.com>
2022-11-10 00:26:21 -0600

    prueba inicial de express

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hola gente');
+});
+
+app.listen(3000);
